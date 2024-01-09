3c1905588682be4a599a2ed82dd2d55e2b3fdd36
Suhun Kim <kksshh404@gmail.com>
2024-01-09 11:46:52 -0800

    Add default setting values with react context

--- app/components/navigation/navigation.tsx
+++ app/components/navigation/navigation.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { IoHome } from "react-icons/io5";
 import { FaPlusCircle } from "react-icons/fa";
 import OpenAndSave from "../buttons/open-and-save";
+
 export default function Nav() {
   return (
     <div className="flex flex-col items-center h-screen bg-black justify-start gap-4 p-1">
@@ -12,6 +13,8 @@ export default function Nav() {
       <OpenAndSave>
         <FaPlusCircle size={30} className="fill-white" />
       </OpenAndSave>
+	  <>
+	  </>
     </div>
   );
 }
--- app/utils/interfaces.ts
+++ app/utils/interfaces.ts
@@ -1,14 +1,33 @@
+import { ReactNode } from "react";
+
 declare global {
   interface Window {
     showOpenFilePicker: any;
   }
 }
+
+export interface Props {
+  children: ReactNode;
+}
+
 export interface WebPImage {
   name: string;
   data: Uint8Array;
 }
+
 export interface FileData {
   base64String: string;
   name: string;
   handle?: FileSystemFileHandle | undefined;
+}
+
+export interface Settings {
+	reversed: boolean;
+	doublePage: boolean;
+	darkmode: boolean;
+}
+
+export type SettingsContextType = {
+  settings: Settings;
+  setSettings: (settings: Settings) => void;
 }
