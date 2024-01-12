acbd1e93c25229deee7750af3ea0719dccf75782
YaleDevUni <rhrnak4403@gmail.com>
2024-01-12 00:23:59 -0800

    Feat: filter by name and added

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import { useLiveQuery } from "dexie-react-hooks";
 import { Book, db } from "./db/db";
 import Image from "next/image";
 import { IoMdAddCircleOutline } from "react-icons/io";
+import { FaCaretDown, FaArrowUp, FaArrowDown } from "react-icons/fa";
 import { useSearchParams } from "next/navigation";
 import { use, useEffect, useState } from "react";
 export default function Home() {
@@ -14,133 +15,229 @@ export default function Home() {
     searchParams.get("page") !== null
       ? parseInt(searchParams.get("page") as string, 10)
       : 1;
+
   const totalBooks = useLiveQuery(() => db.books.count()) ?? 0;
   const [books, setBooks] = useState<Book[] | undefined>(undefined);
-  useLiveQuery(
-    () =>
+  const [toggleDropdown, setToggleDropdown] = useState<boolean>(false);
+
+  const handleToggleDropdown = () => {
+    console.log("toggle");
+    setToggleDropdown(!toggleDropdown);
+  };
+  const handleCloseDropdown = () => {
+    setToggleDropdown(false);
+  };
+
+  useLiveQuery(() => {
+    if (searchParams.get("direction") === "desc") {
       db.books
-        .toCollection()
+        .offset(BOOKS_PER_PAGE * (page - 1))
+        .limit(BOOKS_PER_PAGE)
+        .sortBy(searchParams.get("sort") ?? "title")
+        .then((result) => {
+          setBooks(result);
+        });
+    } else {
+      db.books
         .reverse()
         .offset(BOOKS_PER_PAGE * (page - 1))
         .limit(BOOKS_PER_PAGE)
-        .toArray()
+        .sortBy(searchParams.get("sort") ?? "title")
         .then((result) => {
-          console.log("rerender", page);
           setBooks(result);
-        }),
-    [totalBooks, searchParams.get("page")]
-  );
+        });
+    }
+  }, [
+    totalBooks,
+    searchParams.get("page"),
+    searchParams.get("sort"),
+    searchParams.get("direction"),
+  ]);
 
   const isLoading = !books;
   // check if there is new book added
   return (
-    <div className="p-2 flex flex-col justify-between h-screen">
-      {isLoading ? (
-        <div className="grid grid-cols-6 gap-4 text-white">
-          {Array.from(Array(5).keys())
-            .map((i) => (
-              <div
-                key={i + "loading"}
-                className="col-span-1 bg-gray-900 rounded-md p-2 w-full"
-                style={{ height: 300 }}
+    <div className="p-2">
+      <div className=" flex justify-end relative mb-2">
+        <button
+          // onBlur={handleToggleDropdown}
+          className="bg-gray-900 rounded-md p-2 flex items-center justify-center px-8 relative hover:bg-gray-800"
+          onClick={() => handleToggleDropdown()}
+        >
+          Sort By{" "}
+          <span>
+            <FaCaretDown />
+          </span>
+        </button>
+        {toggleDropdown ? (
+          <ul className=" absolute  top-11 ">
+            <li className=" w-40">
+              <Link
+                href={{
+                  pathname: "/",
+                  query: {
+                    page,
+                    sort: "title",
+                    direction:
+                      searchParams.get("sort") === "title" &&
+                      searchParams.get("direction") === "asc"
+                        ? "desc"
+                        : "asc",
+                  },
+                }}
+                className="bg-gray-900 rounded-t-md py-1 w-full flex items-center justify-center hover:bg-gray-800"
               >
-                loading...
-              </div>
-            ))
-            .concat(
-              <div
-                key={"addBookLoading"}
-                className=" col-span-1 bg-gray-900 rounded-md p-2 flex items-center justify-center "
+                Name{" "}
+                <span className="ml-4">
+                  {searchParams.get("sort") === "title" &&
+                  searchParams.get("direction") === "asc" ? (
+                    <FaArrowUp />
+                  ) : (
+                    <FaArrowDown />
+                  )}
+                </span>
+              </Link>
+            </li>
+            {/* <hr className=" bg-white opacity-30" /> */}
+            <li className=" w-40">
+              <Link
+                href={{
+                  pathname: "/",
+                  query: {
+                    page,
+                    sort: "id",
+                    direction:
+                      searchParams.get("sort") === "id" &&
+                      searchParams.get("direction") === "asc"
+                        ? "desc"
+                        : "asc",
+                  },
+                }}
+                className="bg-gray-900 rounded-b-md py-1 w-full flex items-center justify-center hover:bg-gray-800"
               >
-                <IoMdAddCircleOutline className="fill-white" size={200} />
-              </div>
-            )}
-        </div>
-      ) : (
-        <>
-          <div className="grid grid-cols-6 gap-4 ">
-            {books
-              ?.map((book) => (
-                <Link
-                  href={{
-                    pathname: "/info",
-                    query: { index: book.id },
-                  }}
-                  key={book.id + "link"}
-                  className="col-span-1 bg-gray-900 rounded-md p-2 "
+                Date Added{" "}
+                <span className="ml-4">
+                  {searchParams.get("sort") === "id" &&
+                  searchParams.get("direction") === "asc" ? (
+                    <FaArrowUp />
+                  ) : (
+                    <FaArrowDown />
+                  )}
+                </span>
+              </Link>
+            </li>
+            <hr className=" bg-white opacity-10" />
+          </ul>
+        ) : null}
+      </div>
+      <div className="flex flex-col justify-between h-[98svh]">
+        {isLoading ? (
+          <div className="grid grid-cols-6 gap-4 text-white">
+            {Array.from(Array(5).keys())
+              .map((i) => (
+                <div
+                  key={i + "loading"}
+                  className="col-span-1 bg-gray-900 rounded-md p-2 w-full"
+                  style={{ height: 300 }}
                 >
-                  <Image
-                    src={URL.createObjectURL(
-                      new Blob([book.image as BlobPart], {
-                        type: "image/webp",
-                      })
-                    )}
-                    alt="book cover"
-                    width={500}
-                    height={200}
-                    key={book.id + "image"}
-                  />
-                  <div
-                    key={book.id + "title"}
-                    className=" text-center text-lg text-white"
-                  >
-                    {book.title.split(".")[0]}
-                  </div>
-                </Link>
+                  loading...
+                </div>
               ))
               .concat(
-                <Link
+                <div
+                  key={"addBookLoading"}
                   className=" col-span-1 bg-gray-900 rounded-md p-2 flex items-center justify-center "
-                  key={"addBookLink"}
-                  href={{
-                    pathname: "/viewer",
-                    query: { index: books.length + 1, new: true },
-                  }}
                 >
                   <IoMdAddCircleOutline className="fill-white" size={200} />
-                </Link>
+                </div>
               )}
           </div>
-        </>
-      )}
-      <div className="text-center">
-        {Array.from({ length: Math.ceil(totalBooks / BOOKS_PER_PAGE) }).map(
-          (_, i) => {
-            const currentPage = page;
-            const pageWithinRange =
-              i + 1 >= currentPage - 5 && i + 1 <= currentPage + 5;
-
-            return pageWithinRange ? (
-              i + 1 === currentPage ? (
-                <span key={i + "page"} className="mx-2 text-lg">
-                  <b>{i + 1}</b>
-                </span>
-              ) : (
-                <Link
-                  key={i + "page"}
-                  href={{
-                    pathname: "/",
-                    query: { page: i + 1 },
-                  }}
-                >
-                  <button className="mx-2">{i + 1}</button>
-                </Link>
-              )
-            ) : null;
-          }
+        ) : (
+          <>
+            <div className="grid grid-cols-6 gap-4 ">
+              {books
+                ?.map((book) => (
+                  <Link
+                    href={{
+                      pathname: "/info",
+                      query: { index: book.id },
+                    }}
+                    key={book.id + "link"}
+                    className="col-span-1 bg-gray-900 rounded-md p-2 "
+                  >
+                    <Image
+                      src={URL.createObjectURL(
+                        new Blob([book.image as BlobPart], {
+                          type: "image/webp",
+                        })
+                      )}
+                      alt="book cover"
+                      width={500}
+                      height={200}
+                      key={book.id + "image"}
+                    />
+                    <div
+                      key={book.id + "title"}
+                      className=" text-center text-lg text-white"
+                    >
+                      {book.title.split(".")[0]}
+                    </div>
+                  </Link>
+                ))
+                .concat(
+                  <Link
+                    className=" col-span-1 bg-gray-900 rounded-md p-2 flex items-center justify-center "
+                    key={"addBookLink"}
+                    href={{
+                      pathname: "/viewer",
+                      query: { index: books.length + 1, new: true },
+                    }}
+                  >
+                    <IoMdAddCircleOutline className="fill-white" size={200} />
+                  </Link>
+                )}
+            </div>
+          </>
         )}
-        <hr className="m-2" />
-        <p>
-          This version supports .jpeg, .webp, .png, and .gif images in Zip file
-          and chromium based desktop browser.
-        </p>
-        <p>
-          Upcomming Feature: Open images by folder, categorize images by tags
-        </p>
-        <p>
-          Version 0.2.1 &copy;{" "}
-          <a href="https://github.com/YaleDevUni">YaleDevUni</a>
-        </p>
+        <div className="text-center">
+          {Array.from({ length: Math.ceil(totalBooks / BOOKS_PER_PAGE) }).map(
+            (_, i) => {
+              const currentPage = page;
+              const pageWithinRange =
+                i + 1 >= currentPage - 5 && i + 1 <= currentPage + 5;
+
+              return pageWithinRange ? (
+                i + 1 === currentPage ? (
+                  <span key={i + "page"} className="mx-2 text-lg">
+                    <b>{i + 1}</b>
+                  </span>
+                ) : (
+                  <Link
+                    key={i + "page"}
+                    href={{
+                      pathname: "/",
+                      query: { page: i + 1 },
+                    }}
+                  >
+                    <button className="mx-2">{i + 1}</button>
+                  </Link>
+                )
+              ) : null;
+            }
+          )}
+          <hr className="m-2" />
+          <p>
+            This version supports .jpeg, .webp, .png, and .gif images in Zip
+            file and chromium based desktop browser.
+          </p>
+          <p>
+            Upcomming Feature: Open images by folder, categorize images by tags
+          </p>
+          <p>
+            Version 0.2.1 &copy;{" "}
+            <a href="https://github.com/YaleDevUni">YaleDevUni</a>
+          </p>
+        </div>
       </div>
     </div>
   );
