eb4f63a2cd5b7252d07b6ce1a52d9ee25a16c053
YaleDevUni <rhrnak4403@gmail.com>
2025-05-07 16:16:21 +0900

    Update page.tsx

--- app/viewer/page.tsx
+++ app/viewer/page.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Viewer from "./viewer"; // adjust the path according to your file structure
-import { useRouter } from "next/navigation";
-import { BookList } from "../components/books/book-list"; // adjust the path according to your file structure
+
 const App: React.FC = () => {
   return <Viewer />;
 };
